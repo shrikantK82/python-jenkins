ce861c2e915845a5a3f2f6a3a289820d3df77f5a
shrikantk <shrikantkalange2003@gmail.com>
2025-01-27 09:43:39 +0000

    h1

---
+++ test/test.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client()
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b"Hello from Flask & Docker")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
